8e0b98fe0760f22de93b9d4309efa13e92f5f2a1
Jason Fehr <jasonmfehr@gmail.com>
2017-05-25 22:54:04 -0500

    finished koans 22, 23, and 24

--- src/ii_collections/_24_ExtensionsOnCollections.kt
+++ src/ii_collections/_24_ExtensionsOnCollections.kt
@@ -1,5 +1,7 @@
 package ii_collections
 
+import com.google.common.collect.Lists
+import com.google.common.collect.Maps
 import util.TODO
 
 fun todoTask24(): Nothing = TODO(
@@ -12,8 +14,27 @@ fun todoTask24(): Nothing = TODO(
 )
 
 fun doSomethingStrangeWithCollection(collection: Collection<String>): Collection<String>? {
-    val groupsByLength = collection.groupBy { s -> todoTask24() }
+    val groupsByLength = Maps.newHashMap<Int, List<String>>()
+    for (s in collection) {
+        var strings: MutableList<String>? = groupsByLength[s.length] as MutableList<String>?
+        if (strings == null) {
+            strings = Lists.newArrayList<String>()
+            groupsByLength.put(s.length, strings)
+        }
+        strings!!.add(s)
+    }
 
-    return groupsByLength.values.maxBy { group -> todoTask24() }
+    var maximumSizeOfGroup = 0
+    for (group in groupsByLength.values) {
+        if (group.size > maximumSizeOfGroup) {
+            maximumSizeOfGroup = group.size
+        }
+    }
+
+    for (group in groupsByLength.values) {
+        if (group.size == maximumSizeOfGroup) {
+            return group
+        }
+    }
+    return null
 }
-
