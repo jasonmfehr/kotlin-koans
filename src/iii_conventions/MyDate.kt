c1a9d6b17d6e150da14aa8ff7757a6bbedf75e97
Jason Fehr <jasonmfehr@gmail.com>
2017-05-28 21:52:45 -0500

    finished koans 25, 26, 27, and 28

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,14 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return year.compareTo(other.year) + month.compareTo(other.month) + dayOfMonth.compareTo(other.dayOfMonth)
+    }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+    fun toInt(): Int = (year * 10000) + (month * 100) + dayOfMonth
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +16,21 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        var curDate = start
+        return object : Iterator<MyDate> {
+            override fun hasNext(): Boolean = curDate <= endInclusive
+
+            override fun next(): MyDate {
+                val tmp = curDate
+                curDate = curDate.nextDay()
+                return tmp
+            }
+        }
+    }
+}
+
+operator fun DateRange.contains(other: MyDate): Boolean {
+    return start.toInt() <= other.toInt() && other.toInt() <= endInclusive.toInt()
+}
